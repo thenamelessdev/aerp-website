83e1f9e92051229d7e53b15fbc311d69793dc6ba
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-08-22 09:04:58 +0200

    Updated get staff to 2 questions

--- get staff.js
+++ get staff.js
@@ -1,19 +1,28 @@
-const question1 = Math.floor(Math.random() * 100) + 1;
-const question2 = Math.floor(Math.random() * 100) + 1;
-const result = question1 + question2;
+const question1 = Math.floor(Math.random() * 10) + 1;
+const question2 = Math.floor(Math.random() * 10) + 1;
+const result1 = question1 + question2;
 
-window.alert("To get staff you just have to answer one question.");
-let answer = window.prompt(`What is ${question1} + ${question2}`);
-answer = Number(answer);
-if(!isNaN(answer)){
-    if(answer == result){
-        window.alert("Congrats on your staff!")
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+const question3 = Math.floor(Math.random() * 10) + 1;
+const question4 = Math.floor(Math.random() * 10) + 1;
+const result2 = question3 + question4;
+
+
+window.alert("You just have to answer 2 questions to get staff.");
+
+answer1 = window.prompt(`What is ${question1} + ${question2}?`);
+answer1 = Number(answer1);
+if(answer1 === result1){
+    window.alert("You answered right, one more question.");
+    answer2 = window.prompt(`What is ${question3} + ${question4}?`);
+    answer2 = Number(answer2);
+    if(answer2 === result2){
+        window.alert("Congrats, you will get staff after pressing OK");
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
     else{
-        window.alert("You answered wrong. No staff for you.");
+        window.alert("You answered wrong, no staff for you.");
     }
 }
 else{
-    window.alert("Please enter a number.");
+    window.alert("You answered wrong, no staff for you.");
 }
