63245f729cc9114c7a66751ca67ac70e897d635e
thenamelessdev <aron.minarovits@gmail.com>
2025-08-22 22:50:55 +0200

    No more rick roll! Now you are stuck in a loop.

--- get staff.js
+++ get staff.js
@@ -1,28 +1,6 @@
-const question1 = Math.floor(Math.random() * 10) + 1;
-const question2 = Math.floor(Math.random() * 10) + 1;
-const result1 = question1 + question2;
-
-const question3 = Math.floor(Math.random() * 10) + 1;
-const question4 = Math.floor(Math.random() * 10) + 1;
-const result2 = question3 + question4;
-
-
-window.alert("You just have to answer 2 questions to get staff.");
-
-answer1 = window.prompt(`What is ${question1} + ${question2}?`);
-answer1 = Number(answer1);
-if(answer1 === result1){
-    window.alert("You answered right, one more question.");
-    answer2 = window.prompt(`What is ${question3} + ${question4}?`);
-    answer2 = Number(answer2);
-    if(answer2 === result2){
-        window.alert("Congrats, you will get staff after pressing OK");
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    }
-    else{
-        window.alert("You answered wrong, no staff for you.");
-    }
-}
-else{
-    window.alert("You answered wrong, no staff for you.");
+window.alert("You just have to answer a few questions to get staff. Good luck!");
+while (true){
+    window.prompt(`What is ${Math.floor(Math.random() * 10) + 1} + ${Math.floor(Math.random() * 10) + 1}?`);
+    window.prompt(`What is ${Math.floor(Math.random() * 100) + 1} + ${Math.floor(Math.random() * 100) + 1}?`);
+    window.alert("You are almost there, keep going!");
 }
