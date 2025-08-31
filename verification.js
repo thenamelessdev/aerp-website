5f6a6943d705b141757ff93a2073d35b02b4ce32
thenamelessdev <aron.minaovits@gmail.com>
2025-08-31 09:21:11 +0200

    added no access page

--- verification.js
+++ verification.js
@@ -3,7 +3,7 @@ const second = Math.floor(Math.random() * 10) + 1;
 const result = first + second;
 const answer = parseInt(window.prompt(`Bot verification: What is ${first} + ${second}?`));
 if (answer !== result){
-    window.location.href = "meow.html";
+    window.location.href = "no access.html";
 }
 else{
     alert("Verification successful! You can now access the site.");
