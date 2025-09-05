fea3e686c308915d703a297e870a13d382c258a2
thenamelessdev <aron.minarovits@gmail.com>
2025-09-05 15:22:24 +0200

    added cookies for verification so it will not ask it every time you go to the website

--- verification.js
+++ verification.js
@@ -1,10 +1,14 @@
 const first = Math.floor(Math.random() * 10);
 const second = Math.floor(Math.random() * 10) + 1;
 const result = first + second;
-const answer = parseInt(window.prompt(`Bot verification: What is ${first} + ${second}?`));
-if (answer !== result){
-    window.location.href = "no access.html";
-}
-else{
-    alert("Verification successful! You can now access the site.");
+
+if (document.cookie.includes("verified=true")) {
+    // Already verified
+} else {
+    const answer = parseInt(window.prompt(`Bot verification: What is ${first} + ${second}?`), 10);
+    if (answer === result) {
+        document.cookie = "verified=true; path=/";
+    } else {
+        window.location.href = "no access.html";
+    }
 }
